d47675a6d74f7d95634ad9ea44e2e9a8a8230102
chenk <chenk@apical.com.cn>
2023-05-25 21:46:26 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+case "$1" in
+"")
+    if [ ! -e $PWD/Makefile ]; then
+        ./bootstrap
+        ./configure \
+        --host=$BUILD_HOST \
+        --prefix=$PROJ_INSTALL_DIR \
+        --disable-shared \
+        --enable-static \
+        --disable-drm
+    fi
+    make -j8
+    make install
+    ;;
+clean)
+    make clean || true
+    ;;
+distclean)
+    make distclean || true
+    rm -rf $PWD/Makefile
+    ;;
+esac
